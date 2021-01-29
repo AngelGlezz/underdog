1885c2e64fbdac3e8020d5d6dd9a2e4304126c9a
Angel <angelglezz.7@gmail.com>
2021-01-29 16:47:27 -0600

    fix responsive

--- js/page.js
+++ js/page.js
@@ -19,5 +19,65 @@ $(document).ready(function(){
         slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: false
-    });
+	});
+	
+	$(".nav-link").click(function(){
+		$('#navbarTogglerDemo02').removeClass('show');
+	});
+
+	$(".navbar-brand").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#cover').offset().top
+        }, 1000);
+        return false;
+	});
+
+	$("#who").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#our-section').offset().top
+        }, 1000);
+        return false;
+	});
+
+	$("#what").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#our-section').offset().top
+        }, 1000);
+        return false;
+	});
+
+	$("#brands").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#brands-section').offset().top
+        }, 1000);
+        return false;
+	});
+
+	$("#clients").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#clients-section').offset().top
+        }, 1000);
+        return false;
+	});
+
+	$("#investors").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#investors-section').offset().top
+        }, 1000);
+        return false;
+	});
+
+	$("#milestones").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#milestones-section').offset().top
+        }, 1000);
+        return false;
+	});
+
+	$("#contact").click(function() {
+		$('body,html').animate({
+            scrollTop: $('#contact-section').offset().top
+        }, 1000);
+        return false;
+	});
 });
